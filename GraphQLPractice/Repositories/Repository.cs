590ebf8b9b37b977b0ce12bd28631f7e6c655150
jane_lin <jane_lin@cmoney.com.tw>
2022-04-05 19:12:12 +0800

    new project

---
+++ GraphQLPractice/Repositories/Repository.cs
@@ -0,0 +1,17 @@
+﻿using static GraphQLPractice.Services.Query;
+
+namespace GraphQLPractice.Repositories
+{
+    public class Repository
+    {
+        public Task<List<Book>> GetBooks()
+        {
+            return Task.FromResult(new List<Book>());
+        }
+
+        public Task<List<Book>> GetBook(string bookName, string author)
+        {
+            return Task.FromResult(new List<Book>() { new Book(Guid.NewGuid(), bookName, new Author(Guid.NewGuid(), author)) });
+        }
+    }
+}
---
+++ GraphQLPractice/Services/Query.cs
@@ -0,0 +1,28 @@
+﻿using GraphQLPractice.Repositories;
+
+namespace GraphQLPractice.Services
+{
+    public class Query
+    {
+        public record Author(Guid Id ,string Name);
+        public record Book(Guid Id, string Title, Author Author);
+        public string Gett()
+        {
+            //套件會幫你把Get去掉 變成t
+            return "Hello";
+        }
+
+        public Task<List<Book>> GetBooks([Service] Repository repository)
+        {
+            // 雖然Qurey class 叫 GetBook 但查詢的query要指定的type name 是Book
+            // 用[Service]判斷你帶入的query 和服務的關係 是不是有依賴注入
+            return repository.GetBooks();
+        }
+
+        public Task<List<Book>> GetBookByVariable(string bookName, string author)
+        {
+            var repository = new Repository();
+            return repository.GetBook(bookName, author);
+        }
+    }
+}
